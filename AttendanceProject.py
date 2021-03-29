4bd994730857c810ce3909c5b3d8196157e6588b
Kabichneu <kabichneu@gmail.com>
2021-03-29 17:57:23 +0545

    complete face recognition

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'AttendImages'
 images = []
@@ -25,6 +26,18 @@ def findEncodings(images):
 
     return encodeList
 
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        namelist = []
+        for line in myDataList:
+            entry = line.split(',')
+            namelist.append(entry[0])
+        if name not in namelist:
+            now = datetime.now()
+            dtstring = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtstring}')
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 cap = cv2.VideoCapture(0)
@@ -46,8 +59,15 @@ while True:
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),2)
+            cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            markAttendance(name)
 
-
+    cv2.imshow('webcam', img)
+    cv2.waitKey(1)
 
 # faceLoc = face_recognition.face_locations(imgBishesh)[0]
 # encodeBishesh = face_recognition.face_encodings(imgBishesh)[0]
